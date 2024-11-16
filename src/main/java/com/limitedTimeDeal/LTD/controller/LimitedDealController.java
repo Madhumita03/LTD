78c6e53e355dd842bb1ec2083273f3fec4fd3578
Madhumita03 <madhumita03@outlook.com>
2024-11-17 02:12:26 +0530

    Update LimitedDealController.java

--- src/main/java/com/limitedTimeDeal/LTD/controller/LimitedDealController.java
+++ src/main/java/com/limitedTimeDeal/LTD/controller/LimitedDealController.java
@@ -1,5 +1,10 @@
 package com.limitedTimeDeal.LTD.controller;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,49 +14,54 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.limitedTimeDeal.LTD.model.Buyer;
 import com.limitedTimeDeal.LTD.model.Deal;
+import com.limitedTimeDeal.LTD.model.Seller;
 import com.limitedTimeDeal.LTD.service.LimitedDealManager;
+import com.limitedTimeDeal.LTD.service.SchedulerTask;
 
 @RestController
 public class LimitedDealController {
-	
+
+    private static final Logger logger = LoggerFactory.getLogger(SchedulerTask.class);
+
+    private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+    
 	@Autowired
 	LimitedDealManager manager;
-	
+
+	//@Scheduled(cron = "* * * 1 * ?")
 	@PostMapping("/create/deal")
 	ResponseEntity createDeal(@RequestBody Deal deal) {
-		
+		logger.info("Cron Task :: Execution Time - {}", dateTimeFormatter.format(LocalDateTime.now()));
 		return manager.handleDealCreation(deal);
 	}
 
-	@GetMapping("/register/seller")
-	String createSeller() 
-	{
-		return manager.createSeller();
+	@PostMapping("/register/seller")
+	ResponseEntity createSeller(@RequestBody Seller seller) {
+		return manager.createSeller(seller);
 	}
-	
-	@GetMapping("/register/user/{name}")
-	String createUser(@PathVariable("name") String name)
-	{
-		return manager.registerUser(name);
+
+	@PostMapping("/register/buyer/{name}")
+	ResponseEntity createBuyer(@RequestBody Buyer buyer, @PathVariable("name") String name) {
+		return manager.registerBuyer(name);
 	}
-	
+
 	@GetMapping("/claim/deal/{dealName}/user/{userId}")
-	ResponseEntity claimDeal(@PathVariable("dealName") String dealName, @PathVariable("userId") String userId)
-	{
+	ResponseEntity claimDeal(@PathVariable("dealName") String dealName, @PathVariable("userId") String userId) {
 		return manager.claimDeal(dealName, userId);
 	}
-	
-	@GetMapping("/modify/deal/{dealName}/seller/{sellerId}") 
+
+	@GetMapping("/modify/deal/{dealName}/seller/{sellerId}")
 	ResponseEntity modifyDeal(@PathVariable("dealName") String dealName, @PathVariable("sellerId") String sellerId,
-			@RequestParam(value="noOfItems",required = false) Integer noOfItems, @RequestParam(value="noOfHours",required = false) Integer noOfHours) {
+			@RequestParam(value = "noOfItems", required = false) Integer noOfItems,
+			@RequestParam(value = "noOfHours", required = false) Integer noOfHours) {
 		return manager.updateNoOfItemsOrNoOfHours(dealName, sellerId, noOfItems, noOfHours);
 	}
-	
+
 	@GetMapping("/end/deal/{dealName}/seller/{sellerId}")
 	ResponseEntity endDeal(@PathVariable("dealName") String dealName, @PathVariable("sellerId") String sellerId) {
 		return manager.endDeal(dealName, sellerId);
 	}
-	
-	
+
 }
